64d761d0251801132e4bb3de73793b98d8a4cefe
liuyonlgong <liuyonglong@qding.me>
2017-08-02 15:29:09 +0800

    提交

--- src/main/java/com/material/server/LoginApi.java
+++ src/main/java/com/material/server/LoginApi.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by liuyonglong on 16/4/19.
  */
